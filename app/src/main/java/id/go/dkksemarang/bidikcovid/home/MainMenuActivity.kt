4aca85f201e4f64eb218bc10f98cc7c25935945a
roningrum <rizkioktavianingrum@gmail.com>
2020-04-22 11:29:39 +0700

    update layout

---
+++ app/src/main/java/id/go/dkksemarang/bidikcovid/home/MainMenuActivity.kt
@@ -0,0 +1,13 @@
+package id.go.dkksemarang.bidikcovid.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import id.go.dkksemarang.bidikcovid.R
+
+class MainMenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+    }
+}
