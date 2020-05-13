51a96137dae8abc520776e14c2a6cf0922f24015
roningrum <rizkioktavianingrum@gmail.com>
2020-05-13 08:54:44 +0700

    update

---
+++ app/src/main/java/id/go/dkksemarang/bidikcovid/ui/home/fragmentpasien/PasienBelumSurveiLokasi.kt
@@ -0,0 +1,23 @@
+package id.go.dkksemarang.bidikcovid.ui.home.fragmentpasien
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import id.go.dkksemarang.bidikcovid.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class PasienBelumSurveiLokasi : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pasien_belum_survei_lokasi, container, false)
+    }
+
+}
