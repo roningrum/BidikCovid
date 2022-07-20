23fa06b0d018b906cb298de30f235ffa5f42c27f
Ota <rizkioktavianingrum@gmail.com>
2022-07-20 11:06:18 +0700

    update edit lokasi tapi data masuk berulang

---
+++ app/src/main/java/id/go/dkksemarang/bidikcovid/EditPasienMapActivity.kt
@@ -0,0 +1,11 @@
+package id.go.dkksemarang.bidikcovid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditPasienMapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_pasien_map)
+    }
+}
